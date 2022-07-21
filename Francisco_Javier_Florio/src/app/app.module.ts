5eb339ee8bf6fceb4b30e4861e6003b7d2283a85
Francisco Florio <francisco.florio25@gmail.com>
2022-07-21 18:34:24 -0300

    actualizacion de angular y creacion de modulo encabezado

--- Francisco_Javier_Florio/src/app/app.module.ts
+++ Francisco_Javier_Florio/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    EncabezadoComponent
   ],
   imports: [
     BrowserModule,
